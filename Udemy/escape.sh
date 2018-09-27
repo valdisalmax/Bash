bd14a7434fa433b699a7b32b2124c781152705ba
Buddy Lodder <b.lodder@bl-ict.nl>
2018-09-27 05:04:15 -0700

    added escape2.sh

--- Udemy/escape.sh
+++ Udemy/escape.sh
@@ -3,6 +3,15 @@
 echo "\"\\Linux\\\" is \\\\ Awesome\\\\"
 
 let val=500/2
+val2=`echo $val`
 echo $val
+echo $val2
+
+var=20
+if [ "$var" -lt 15 ]
+then :
+else
+	echo $var
+fi
 
 
---
+++ Udemy/escape2.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+var=0
+
+if [ "$var" != 0 ]
+then
+	echo "NOT 0"
+else
+	echo "YES 0"
+fi
